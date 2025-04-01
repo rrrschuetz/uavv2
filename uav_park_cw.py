e20568dcb15fdca0e83042527f092b5659970f9f
rrrschuetz <rrrschuetz@web.de>
2025-04-01 17:36:21 +0200

    magnetometer based lap counting

--- uav_park_cw.py
+++ uav_park_cw.py
@@ -610,8 +610,8 @@ def detect_and_label_blobs(image, num_detector_calls):
         contours, _ = cv2.findContours(magenta_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
         for contour in contours:
             area = cv2.contourArea(contour)
-            if area > 5000:
-                #print(f"Magenta rectangle detected: {area} pixels")
+            if area > 10000:  #5000 size of parking lot
+                print(f"Magenta rectangle detected: {area} pixels")
                 magenta_rectangle = True
                 cv2.drawContours(image, [contour], -1, (255, 255, 255), 2)  # Draw the magenta rectangle
 
@@ -932,10 +932,11 @@ def gyro_thread(shared_race_mode):
                 else:
                     # Get the magnetometer heading (absolute heading)
                     Gheading_estimate = get_magnetometer_heading()
-                    Glap_end = abs(yaw_difference(Gheading_estimate, Gheading_start)) < 10
+                    yaw_diff = abs(yaw_difference(Gheading_estimate, Gheading_start))
+                    Glap_end = yaw_diff < 10
                     #Gparallel_aligned = abs(orientation(Gheading_estimate) - orientation(Gheading_start)) < 10
-                    Gparallel_aligned = (abs(yaw_difference(Gheading_estimate, Gheading_start)) % 90)  < 10
-                    print(f"Gparallel_aligned: {Gparallel_aligned} Glap_end: {Glap_end} Gheading_estimate: {Gheading_estimate:.2f}")
+                    Gparallel_aligned = (abs(yaw_diff % 90)  < 10
+                    print(f"Gparallel_aligned: {Gparallel_aligned} Glap_end: {Glap_end} Gheading_estimate: {Gheading_estimate:.2f} yaw_diff: {yaw_diff:.2f}  {yaw_diff % 90:.2f} ")
                     time.sleep(0.1)
 
     except serial.SerialException as e:
