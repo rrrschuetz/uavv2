a5c837280c9796629b825a0f6b8ac3a1fa3f7671
rrrschuetz <rrrschuetz@web.de>
2025-05-30 21:42:23 +0200

    bug fixing

--- uav_park_lab.py
+++ uav_park_lab.py
@@ -619,7 +619,7 @@ class mask():
     #    return magenta_mask
 
     def magenta(self, image):
-        lower_magenta = np.array([0, 0, 130])   #20,150,150
+        lower_magenta = np.array([0, 150, 130])   #20,150,150
         upper_magenta = np.array([255, 255, 255])
         magenta_mask = cv2.inRange(image, lower_magenta, upper_magenta)
         return magenta_mask
